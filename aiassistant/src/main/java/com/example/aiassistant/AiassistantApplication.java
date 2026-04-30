6df1bc49f82aeac1e22b07129a81a0b2a6ce615b
susmitha7599 <susmitha7599@gmail.com>
2026-04-30 17:03:06 -0400

    This is my first commit

---
+++ aiassistant/src/main/java/com/example/aiassistant/AiassistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiassistantApplication.class, args);
+	}
+
+}
